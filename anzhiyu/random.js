b190f88428a830619cc7e718c92c2a4a9a4d27b3
Txy-Sky <1580264151@qq.com>
2024-11-27 23:12:55 +0800

    Site updated: 2024-11-27 23:12:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined-Txy/图片的博客/","undefined-Txy/包含图片的博客/","undefined-Txy/这是包含图片的博客/","undefined-Txy/这是我的第一篇博客/","undefined-Txy/hello-world/"];function toRandomPost(){
+var posts=["undefined-Txy/图片的博客/","undefined-Txy/包含图片的博客/","undefined-Txy/这是包含图片的博客/","undefined-Txy/这是我的第一篇博客/","undefined-Txy/hello-world/","undefined-Txy/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
