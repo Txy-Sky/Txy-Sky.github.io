fca005b31f2c93289f86697d641d6e4371dbfae6
Txy-Sky <1580264151@qq.com>
2024-12-02 20:38:00 +0800

    Site updated: 2024-12-02 20:38:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined-Txy/渗透测试笔记思路/","undefined-Txy/头像/","undefined-Txy/hello-world/","undefined-Txy/测试/"];function toRandomPost(){
+var posts=["undefined-Txy/未命名/","undefined-Txy/渗透测试笔记思路/","undefined-Txy/头像/","undefined-Txy/hello-world/","undefined-Txy/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
