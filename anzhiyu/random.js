3bd8f48a96348fedc4f74ae16c27b2bf22509470
Txy-Sky <1580264151@qq.com>
2024-12-08 20:30:21 +0800

    Site updated: 2024-12-08 20:30:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/29/渗透测试笔记思路/","2024/11/26/头像/","2024/11/25/hello-world/","2024/07/05/测试/"];function toRandomPost(){
+var posts=["2024/12/08/CTF-逆向题-pyc思路/","2024/11/29/渗透测试笔记思路/","2024/11/25/hello-world/","2024/07/05/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
